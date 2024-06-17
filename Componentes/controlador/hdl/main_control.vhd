b84f3a6c34112874bcdcb1843c37ebf8670c7cbb
Levy Elmescany <levyelmescany@edu.univali.br>
2024-06-16 22:18:09 -0300

    Atualização Controlador Principal

--- Componentes/controlador/hdl/main_control.vhd
+++ Componentes/controlador/hdl/main_control.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity main_control is
 port ( i_OP       : in  std_logic_vector(6 downto 0);
-       o_ALUOp    : out  std_logic_vector(1 downto 0);     
+       o_ALU_OP   : out  std_logic_vector(1 downto 0);     
        o_ALUSRC   : out std_logic;   
        o_MEMTOREG : out std_logic;   
        o_REGWRITE : out std_logic;   
@@ -16,15 +16,15 @@ architecture arch_1 of main_control is
 begin
   process(i_OP)
   begin
-    o_ALUOp(0) <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and i_OP(6);
-    o_ALUOp(1) <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and  i_OP(4) and i_OP(5) and (not i_OP(6));
-    o_ALUSRC   <= (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6))) or 
-                  (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and (not i_OP(6)));
-    o_MEMTOREG <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6));   
-    o_REGWRITE <= (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and  i_OP(4) and i_OP(5) and (not i_OP(6))) or 
-                  (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6)));
-    o_MEMREAD  <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6));    
-    o_MEMWRITE <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and (not i_OP(6)); 
-    o_BRANCH   <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and i_OP(6);
+    o_ALU_OP(0) <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and i_OP(6);
+    o_ALU_OP(1) <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and  i_OP(4) and i_OP(5) and (not i_OP(6));
+    o_ALUSRC    <= (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6))) or 
+                   (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and (not i_OP(6)));
+    o_MEMTOREG  <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6));   
+    o_REGWRITE  <= (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and  i_OP(4) and i_OP(5) and (not i_OP(6))) or 
+                   (i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6)));
+    o_MEMREAD   <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and (not i_OP(5)) and (not i_OP(6));    
+    o_MEMWRITE  <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and (not i_OP(6)); 
+    o_BRANCH    <=  i_OP(0) and i_OP(1) and (not i_OP(2)) and (not i_OP(3)) and (not i_OP(4)) and i_OP(5) and i_OP(6);
   end process;
 end arch_1;
--- Componentes/controlador/hdl/tb_main_control.vhd
+++ Componentes/controlador/hdl/tb_main_control.vhd
@@ -1,77 +1,72 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-entity tb_Main_control is
-end tb_Main_control;
+entity tb_main_control is
+end tb_main_control;
 
-architecture behavioral of tb_Main_control is
-    -- Component declaration for the Unit Under Test (UUT)
-    component Main_control
-        port (
-            i_OP     : in  std_logic_vector(6 downto 0);
-            o_ALUOp  : out std_logic_vector(1 downto 0);  
-            o_ALUSRC : out std_logic;   
-            o_MEMTOREG : out std_logic;   
-            o_REGWRITE : out std_logic;   
-            o_MEMREAD : out std_logic;   
-            o_MEMWRITE : out std_logic;   
-            o_BRANCH : out std_logic  
-        );
-    end component;
+architecture behavioral of tb_main_control is
+  -- Declaração de DUV
+  component main_control
+  port ( i_OP       : in  std_logic_vector(6 downto 0);
+         o_ALU_OP   : out std_logic_vector(1 downto 0);  
+         o_ALUSRC   : out std_logic;   
+         o_MEMTOREG : out std_logic;   
+         o_REGWRITE : out std_logic;   
+         o_MEMREAD  : out std_logic;   
+         o_MEMWRITE : out std_logic;   
+         o_BRANCH   : out std_logic);
+  end component;
 
-    -- Signals to connect to the UUT
-    signal i_OP     : std_logic_vector(6 downto 0);
-    signal o_ALUOp  : std_logic_vector(1 downto 0);  
-    signal o_ALUSRC : std_logic;   
-    signal o_MEMTOREG : std_logic;   
-    signal o_REGWRITE : std_logic;   
-    signal o_MEMREAD : std_logic;   
-    signal o_MEMWRITE : std_logic;   
-    signal o_BRANCH : std_logic;
+  --Declaração de Sinais
+  signal w_OP       : std_logic_vector(6 downto 0);
+  signal w_ALU_OP   : std_logic_vector(1 downto 0);  
+  signal w_ALUSRC   : std_logic;   
+  signal w_MEMTOREG : std_logic;   
+  signal w_REGWRITE : std_logic;   
+  signal w_MEMREAD  : std_logic;   
+  signal w_MEMWRITE : std_logic;   
+  signal w_BRANCH   : std_logic;
 
 begin
-    -- Instantiate the Unit Under Test (UUT)
-    uut: Main_control
-        port map (
-            i_OP => i_OP,
-            o_ALUOp => o_ALUOp,
-            o_ALUSRC => o_ALUSRC,
-            o_MEMTOREG => o_MEMTOREG,
-            o_REGWRITE => o_REGWRITE,
-            o_MEMREAD => o_MEMREAD,
-            o_MEMWRITE => o_MEMWRITE,
-            o_BRANCH => o_BRANCH
-        );
+  -- Conectando DUV
+  u_DUV: main_control port map ( i_OP       => w_OP,
+                                 o_ALU_OP    => w_ALU_OP,
+                                 o_ALUSRC   => w_ALUSRC,
+                                 o_MEMTOREG => w_MEMTOREG,
+                                 o_REGWRITE => w_REGWRITE,
+                                 o_MEMREAD  => w_MEMREAD,
+                                 o_MEMWRITE => w_MEMWRITE,
+                                 o_BRANCH   => w_BRANCH);
 
-    -- Stimulus process
-    stim_proc: process
-    begin
-        -- Test case: R-type instruction (i_OP = "0110011")
-        i_OP <= "0110011";
-        wait for 10 ns;
-        assert (o_ALUOp = "10" and o_ALUSRC = '0' and o_MEMTOREG = '0' and o_REGWRITE = '1' and o_MEMREAD = '0' and o_MEMWRITE = '0' and o_BRANCH = '0')
-            report "Test case for R-type instruction failed" severity error;
+  process
+  begin
+    -- Test case: R-type instruction (i_OP = "0110011")
+    w_OP <= "0110011";
+    wait for 1 ns;
+    assert (w_ALU_OP = "10" and w_ALUSRC = '0' and w_MEMTOREG = '0' and w_REGWRITE = '1' and w_MEMREAD = '0' and w_MEMWRITE = '0' and w_BRANCH = '0')
+    report "Test case for R-type instruction failed" severity error;
 
-        -- Test case: I-type (Load) instruction (i_OP = "0000011")
-        i_OP <= "0000011";
-        wait for 10 ns;
-        assert (o_ALUOp = "00" and o_ALUSRC = '1' and o_MEMTOREG = '1' and o_REGWRITE = '1' and o_MEMREAD = '1' and o_MEMWRITE = '0' and o_BRANCH = '0')
-            report "Test case for I-type Load instruction failed" severity error;
+    -- Test case: I-type (Load) instruction (i_OP = "0000011")
+    w_OP <= "0000011";
+    wait for 1 ns;
+    assert (w_ALU_OP = "00" and w_ALUSRC = '1' and w_MEMTOREG = '1' and w_REGWRITE = '1' and w_MEMREAD = '1' and w_MEMWRITE = '0' and w_BRANCH = '0')
+    report "Test case for I-type Load instruction failed" severity error;
 
-        -- Test case: S-type (Store) instruction (i_OP = "0100011")
-        i_OP <= "0100011";
-        wait for 10 ns;
-        assert (o_ALUOp = "00" and o_ALUSRC = '1' and o_MEMTOREG = '0' and o_REGWRITE = '0' and o_MEMREAD = '0' and o_MEMWRITE = '1' and o_BRANCH = '0')
-            report "Test case for S-type Store instruction failed" severity error;
+    -- Test case: S-type (Store) instruction (i_OP = "0100011")
+    w_OP <= "0100011";
+    wait for 1 ns;
+    assert (w_ALU_OP = "00" and w_ALUSRC = '1' and w_MEMTOREG = '0' and w_REGWRITE = '0' and w_MEMREAD = '0' and w_MEMWRITE = '1' and w_BRANCH = '0')
+    report "Test case for S-type Store instruction failed" severity error;
 
-        -- Test case: B-type (Branch) instruction (i_OP = "1100011")
-        i_OP <= "1100011";
-        wait for 10 ns;
-        assert (o_ALUOp = "01" and o_ALUSRC = '0' and o_MEMTOREG = '0' and o_REGWRITE = '0' and o_MEMREAD = '0' and o_MEMWRITE = '0' and o_BRANCH = '1')
-            report "Test case for B-type Branch instruction failed" severity error;
+    -- Test case: B-type (Branch) instruction (i_OP = "1100011")
+    w_OP <= "1100011";
+    wait for 1 ns;
+    assert (w_ALU_OP = "01" and w_ALUSRC = '0' and w_MEMTOREG = '0' and w_REGWRITE = '0' and w_MEMREAD = '0' and w_MEMWRITE = '0' and w_BRANCH = '1')
+    report "Test case for B-type Branch instruction failed" severity error;
 
-        -- End of simulation
-        wait;
-    end process;
-
+    -- Clear Inputs
+    w_OP <= (others => '0');
+	  assert false report "Teste concluído com sucesso." severity note;
+    wait;
+  end process;
 end architecture behavioral;
